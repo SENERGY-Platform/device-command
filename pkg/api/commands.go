443e597b69db1d244fc27de48b1963b66c7a9f8c
Ingo <roessner@infai.org>
2022-01-28 09:44:49 +0100

    implement batch commands

--- pkg/api/commands.go
+++ pkg/api/commands.go
@@ -22,24 +22,11 @@ import (
 	"github.com/SENERGY-Platform/device-command/pkg/configuration"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"sync"
 )
 
 func init() {
 	endpoints = append(endpoints, CommandEndpoints)
-}
-
-type CommandMessage struct {
-	FunctionId string      `json:"function_id"` //mandatory
-	Input      interface{} `json:"input"`
-
-	//device command
-	DeviceId  string `json:"device_id,omitempty"`
-	ServiceId string `json:"service_id,omitempty"`
-
-	//group command
-	GroupId       string `json:"group_id,omitempty"`
-	AspectId      string `json:"aspect_id,omitempty"`
-	DeviceClassId string `json:"device_class_id,omitempty"`
 }
 
 func CommandEndpoints(config configuration.Config, router *httprouter.Router, command Command) {
@@ -80,4 +67,69 @@ func CommandEndpoints(config configuration.Config, router *httprouter.Router, co
 		http.Error(writer, "missing device_id, service_id or group_id", http.StatusBadRequest)
 
 	})
+
+	router.POST("/commands/batch", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		token, err := auth.GetParsedToken(request)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+		batch := BatchRequest{}
+		err = json.NewDecoder(request.Body).Decode(&batch)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = batch.Validate()
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		result := runBatch(token, command, batch)
+		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(writer).Encode(result)
+		return
+	})
+}
+
+func runBatch(token auth.Token, command Command, batch BatchRequest) []BatchResultElement {
+	if len(batch) == 0 {
+		return []BatchResultElement{}
+	}
+
+	result := make([]BatchResultElement, len(batch))
+	wg := sync.WaitGroup{}
+	mux := sync.Mutex{}
+
+	for i, cmd := range batch {
+		wg.Add(1)
+		go func(i int, cmd CommandMessage) {
+			defer wg.Done()
+			if cmd.DeviceId != "" && cmd.ServiceId != "" {
+				code, temp := command.DeviceCommand(token, cmd.DeviceId, cmd.ServiceId, cmd.FunctionId, cmd.Input)
+				mux.Lock()
+				defer mux.Unlock()
+				result[i] = BatchResultElement{
+					StatusCode: code,
+					Message:    temp,
+				}
+				return
+			}
+
+			if cmd.GroupId != "" {
+				code, temp := command.GroupCommand(token, cmd.GroupId, cmd.FunctionId, cmd.AspectId, cmd.DeviceClassId, cmd.Input)
+				mux.Lock()
+				defer mux.Unlock()
+				result[i] = BatchResultElement{
+					StatusCode: code,
+					Message:    temp,
+				}
+				return
+			}
+		}(i, cmd)
+	}
+	wg.Wait()
+	return result
 }
---
+++ pkg/api/messages.go
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package api
+
+import (
+	"errors"
+	"strconv"
+)
+
+type CommandMessage struct {
+	FunctionId string      `json:"function_id"` //mandatory
+	Input      interface{} `json:"input"`
+
+	//device command
+	DeviceId  string `json:"device_id,omitempty"`
+	ServiceId string `json:"service_id,omitempty"`
+
+	//group command
+	GroupId       string `json:"group_id,omitempty"`
+	AspectId      string `json:"aspect_id,omitempty"`
+	DeviceClassId string `json:"device_class_id,omitempty"`
+}
+
+func (this CommandMessage) Validate() error {
+	if this.FunctionId == "" {
+		return errors.New("expect function_id in body")
+	}
+
+	if this.DeviceId != "" && this.ServiceId != "" {
+		return nil
+	}
+
+	if this.GroupId != "" {
+		return nil
+	}
+
+	return errors.New("missing device_id, service_id or group_id")
+}
+
+type BatchRequest []CommandMessage
+
+func (this BatchRequest) Validate() error {
+	for i, req := range this {
+		err := req.Validate()
+		if err != nil {
+			return errors.New("[" + strconv.Itoa(i) + "]: " + err.Error())
+		}
+	}
+	return nil
+}
+
+type BatchResultElement struct {
+	StatusCode int         `json:"status_code"`
+	Message    interface{} `json:"message"`
+}
